7d4660ce985db266cc14f5760f7498ad713c3762
Altinai333 <atoucheteau@putnamscience.org>
2025-04-22 13:55:11 -0400

    start
    basics of the final proyect

---
+++ LIBRARY.py
@@ -0,0 +1,9 @@
+
+
+from storage import library
+
+b=library()
+print(b.name)
+
+c=b.add()
+v=input("would you like to add  a  book")
---
+++ storage.py
@@ -0,0 +1,11 @@
+
+
+class library():
+    def __init__(self):
+        self.name ="Lina"
+        self.book=""
+
+    def add(self):
+        self.book=input("what book would you like to buy?")
+        print("you have added",self.book)
+        
