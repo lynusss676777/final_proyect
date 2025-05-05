d59019d30e030a523c7f47566c5c136ba960c415
Altinai333 <atoucheteau@putnamscience.org>
2025-05-05 14:20:38 -0400

    doing the file

--- LIBRARY.py
+++ LIBRARY.py
@@ -4,12 +4,17 @@ from storage import library
 
 b=library()
 v=""
-print(b.book)
+
 
 
 while v !="quit":
-    v = input("would you like to add a book?\n1. for yes \n2 for no\nEnter choice (1/2): " )
-    if v=="1":
-            c=b.add
-    elif v=="2":
-          c=b.sell()
+        v = input("would you like to add a book?\n1. for yes \n2 for no\n3.show library\n4. create a file\nEnter choice (1/2/3/4): " )
+        if v=="1":
+                c=b.add()
+        elif v=="2":
+                c=b.sell()
+        elif v=="3":
+                c=b.showboook()
+        elif v=="4":
+                c=b.data()
+                
--- storage.py
+++ storage.py
@@ -1,6 +1,7 @@
 
 
 class library():
+    f = open("demofile.txt")
     def __init__(self):
         self.author =""
         self.name=""
@@ -13,7 +14,7 @@ class library():
        self.tittle=input("Enter the name of the book you would like to add\n").upper()
        self.author=input("Enter the Author of the book you have just added\n").upper()
        self.price=float(input("Enter the listing price of the book\n"))
-       self.books[self.tittle]={"Author":self.author,"price":self.price}
+       self.books[self.tittle]={'Author':self.author,'price':self.price}
        print(self.books)
 
     def sell(self):
@@ -30,8 +31,24 @@ class library():
     def showboook(self):
         for x in self.books:
             print("\nBook title",x,"") 
-                  
-                  
+            print(self.books[x]['Author'])     
+            print(self.books[x]['price'])     
 
-      
-   
+    def options (self):
+        print(self.showboook)
+        myfile = open('myfile.txt', 'w')
+        self.books[self.title] = {"writer":self.author, "price": self.price}
+        for a in self.books:
+            myfile.write(a)
+            myfile.write("\n")
+            myfile.write(self.books[a]["writer"])
+            myfile.write("\n")
+            myfile.write(self.books[a]["price"])
+            myfile.write("\n")
+            myfile.close()
+            myfile.write("\n")
+            print(myfile)
+
+
+
+
